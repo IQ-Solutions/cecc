2cdd3fa1619c444bd5b266208fc321afd98c5f39
krice <krice@iqsolutions.com>
2021-08-19 16:56:05 -0400

    completes order over limit display

--- modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/OverLimit.php
+++ modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/OverLimit.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\cecc_checkout\Plugin\Commerce\CheckoutPane;
 
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutFlow\CheckoutFlowInterface;
 use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneBase;
-use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Utility\Token;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Adds customer questions to checkout summary.
@@ -16,7 +18,104 @@ use Drupal\Core\Form\FormStateInterface;
  *   wrapper_element = "fieldset",
  * )
  */
-class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
+class OverLimit extends CheckoutPaneBase {
+
+  /**
+   * The token service.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, CheckoutFlowInterface $checkout_flow = NULL) {
+    $instance = new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $checkout_flow,
+      $container->get('entity_type.manager')
+    );
+    $instance->setToken($container->get('token'));
+    return $instance;
+  }
+
+  /**
+   * Sets the token service.
+   *
+   * @param \Drupal\Core\Utility\Token $token
+   *   The token service.
+   */
+  public function setToken(Token $token) {
+    $this->token = $token;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'message' => [
+        'value' => '<p>Requests for quantities above the limit are considered on a case-by-case basis. Please call the NINDS toll-free number <a href="tel:8003529424">800-352-9424</a> between 8:30 a.m. and 5:00 p.m. Eastern time, Monday through Friday, to place your order and explain how you plan to use our materials.</p><p>You have requested a quantity of product(s) NDS-169 greater than the limit we allow. You can lower your requested quantity OR We may be able to ship the full amount you have requested, but need more information.</p>',
+        'format' => 'basic_html',
+      ],
+      'form_message' => [
+        'value' => '<p><strong>Please complete this form to request an over the limit quantity</strong></p><p>Please provide the list of publications, quantities, how they will be used, and the expected audience. We will makes every attempt to ship the amount you requested, however you may receive a smaller amount based on supply.</p>',
+        'format' => 'basic_html',
+      ],
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['message'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Over limit message'),
+      '#description' => $this->t('Over limit message text'),
+      '#default_value' => $this->configuration['message']['value'],
+      '#format' => $this->configuration['message']['format'],
+      '#element_validate' => ['token_element_validate'],
+      '#token_types' => ['commerce_order'],
+      '#required' => TRUE,
+    ];
+
+    $form['form_message'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Form over limit message'),
+      '#description' => $this->t('Over limit message text display while editing'),
+      '#default_value' => $this->configuration['form_message']['value'],
+      '#format' => $this->configuration['form_message']['format'],
+      '#element_validate' => ['token_element_validate'],
+      '#token_types' => ['commerce_order'],
+      '#required' => TRUE,
+    ];
+
+    $form['token_help'] = [
+      '#theme' => 'token_tree_link',
+      '#token_types' => ['commerce_order'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    if (!$form_state->getErrors()) {
+      $values = $form_state->getValue($form['#parents']);
+      $this->configuration['message'] = $values['message'];
+      $this->configuration['form_message'] = $values['form_message'];
+    }
+  }
 
   /**
    * {@inheritDoc}
@@ -41,6 +140,22 @@ class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
    * {@inheritDoc}
    */
   public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $message = $this->token->replace($this->configuration['message']['value'], [
+      'commerce_order' => $this->order,
+    ]);
+    $formMessage = $this->token->replace($this->configuration['form_message']['value'], [
+      'commerce_order' => $this->order,
+    ]);
+
+    $pane_form['message'] = [
+      '#type' => 'markup',
+      '#markup' => $message,
+    ];
+
+    $pane_form['form_message'] = [
+      '#type' => 'markup',
+      '#markup' => $formMessage,
+    ];
 
     $pane_form['field_event_name'] = [
       '#type' => 'textfield',
@@ -83,12 +198,33 @@ class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
   public function buildPaneSummary() {
     $build = [];
 
+    $message = $this->token->replace($this->configuration['message']['value'], [
+      'commerce_order' => $this->order,
+    ]);
+
     if (!$this->order->get('field_cecc_over_limit_desc')->isEmpty()) {
       $build = [
         'summary_display' => [
           '#type' => 'container',
           '#title' => $this->t('Over Limit Order Request'),
         ],
+      ];
+      $build['summary_display']['message'] = [
+        '#type' => 'markup',
+        '#markup' => $message,
+        //'#theme' => 'commerce_checkout_completion_message',
+        //'#order_entity' => $this->order,
+        //'#message' => [
+        //  '#type' => 'processed_text',
+        //  '#text' => $message,
+        //  '#format' => $this->configuration['message']['format'],
+        //],
+      ];
+
+      $build['summary_display']['field_event_name'] = [
+        '#type' => 'item',
+        '#title' => 'Event name',
+        '#markup' => '<p>' . $this->order->get('field_event_name')->value . '</p>',
       ];
 
       $build['summary_display']['field_event_name'] = [
