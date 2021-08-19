7afd65ec9b5e111c6d14213960dab7435802552c
krice <krice@iqsolutions.com>
2021-08-19 16:03:19 -0400

    adds over limit value

--- modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/OverLimit.php
+++ modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/OverLimit.php
@@ -11,7 +11,7 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @CommerceCheckoutPane(
  *   id = "cecc_overlimit",
- *   label = @Translation("Item Over Limit Notes"),
+ *   label = @Translation("Over Limit Order Request"),
  *   default_step = "order_notes",
  *   wrapper_element = "fieldset",
  * )
@@ -21,11 +21,44 @@ class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
   /**
    * {@inheritDoc}
    */
+  public function isVisible() {
+    $orderItems = $this->order->getItems();
+
+    foreach ($orderItems as $orderItem) {
+      $quantity = $orderItem->getQuantity();
+      $purchasedEntity = $orderItem->getPurchasedEntity();
+      $overLimitValue = $purchasedEntity->get('field_cecc_order_limit')->value;
+
+      if (!empty($overLimitValue) && $quantity > $overLimitValue) {
+        return TRUE;
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+
+    $pane_form['field_event_name'] = [
+      '#type' => 'textfield',
+      '#title' => 'Event name',
+      '#default_value' => $this->order->get('field_event_name')->isEmpty() ?
+      NULL : $this->order->get('field_event_name')->value,
+    ];
+
+    $pane_form['field_event_location'] = [
+      '#type' => 'textfield',
+      '#title' => 'Event location (state)',
+      '#default_value' => $this->order->get('field_event_location')->isEmpty() ?
+      NULL : $this->order->get('field_event_location')->value,
+    ];
 
     $pane_form['field_cecc_over_limit_desc'] = [
       '#type' => 'textarea',
-      '#title' => $this->order->get('field_cecc_over_limit_desc')->getFieldDefinition()->getLabel(),
+      '#title' => 'Description',
       '#default_value' => $this->order->get('field_cecc_over_limit_desc')->isEmpty() ?
       NULL : $this->order->get('field_cecc_over_limit_desc')->value,
       '#required' => TRUE,
@@ -39,6 +72,8 @@ class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
    */
   public function submitPaneForm(array &$pane_form, FormStateInterface $form_state, array &$complete_form) {
     $value = $form_state->getValue($pane_form['#parents']);
+    $this->order->set('field_event_name', $value['field_event_name']);
+    $this->order->set('field_event_location', $value['field_event_location']);
     $this->order->set('field_cecc_over_limit_desc', $value['field_cecc_over_limit_desc']);
   }
 
@@ -52,8 +87,20 @@ class OverLimit extends CheckoutPaneBase implements CheckoutPaneInterface {
       $build = [
         'summary_display' => [
           '#type' => 'container',
-          '#title' => $this->t('Item Over Limit Notes'),
+          '#title' => $this->t('Over Limit Order Request'),
         ],
+      ];
+
+      $build['summary_display']['field_event_name'] = [
+        '#type' => 'item',
+        '#title' => 'Event name',
+        '#markup' => '<p>' . $this->order->get('field_event_name')->value . '</p>',
+      ];
+
+      $build['summary_display']['field_event_location'] = [
+        '#type' => 'item',
+        '#title' => 'Event location (state)',
+        '#markup' => '<p>' . $this->order->get('field_event_location')->value . '</p>',
       ];
 
       $build['summary_display']['field_cecc_over_limit_desc'] = [
