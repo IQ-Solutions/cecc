01e25a0eadc888eeefc5700f1e190e6de5ba6a0d
krice <krice@iqsolutions.com>
2021-08-26 12:10:33 -0400

    fixes shipping method display glitch

--- modules/cecc_shipping/src/Service/ShipmentWeightCalculator.php
+++ modules/cecc_shipping/src/Service/ShipmentWeightCalculator.php
@@ -3,12 +3,41 @@
 namespace Drupal\cecc_shipping\Service;
 
 use Drupal\commerce_price\Price;
+use Drupal\commerce_price\Rounder;
 use Drupal\commerce_shipping\Entity\ShipmentInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ShipmentWeightCalculator.
  */
-class ShipmentWeightCalculator {
+class ShipmentWeightCalculator implements ContainerInjectionInterface {
+
+  /**
+   * Commerce price rounder.
+   *
+   * @var \Drupal\commerce_price\Rounder
+   */
+  protected $priceRounder;
+
+  /**
+   * Shipment Weight calculator constructor.
+   *
+   * @param \Drupal\commerce_price\Rounder $rounder
+   *   The price rounder service.
+   */
+  public function __construct(Rounder $rounder) {
+    $this->priceRounder = $rounder;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('commerce_price.rounder')
+    );
+  }
 
   /**
    * Calculate the price of a shipment based on weight.
@@ -25,8 +54,10 @@ class ShipmentWeightCalculator {
    */
   public function calculate(ShipmentInterface $shipment, Price $basePrice, Price $weightPrice) {
     $weight = $shipment->getWeight()->convert('lb')->getNumber();
-    return $weightPrice->multiply($weight)
+    $price = $weightPrice->multiply($weight)
       ->add($basePrice);
+
+    return $this->priceRounder->round($price);
   }
 
 }
