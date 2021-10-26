b080a16d8e40673731ce0277616eff704ea575fa
krice <krice@iqsolutions.com>
2021-10-26 14:16:35 -0400

    removes shipping fields

--- modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/CustomerQuestions.php
+++ modules/cecc_checkout/src/Plugin/Commerce/CheckoutPane/CustomerQuestions.php
@@ -21,25 +21,7 @@ class CustomerQuestions extends CheckoutPaneBase implements CheckoutPaneInterfac
   /**
    * {@inheritDoc}
    */
-  public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form)
-  {
-
-    $pane_form['field_shipping_vendor'] = [
-      '#type' => 'select',
-      '#title' => $this->order->get('field_shipping_vendor')->getFieldDefinition()->getLabel(),
-      '#options' => $this->order->get('field_shipping_vendor')->getSetting('allowed_values'),
-      '#empty_option' => '- Select a value -',
-      '#default_value' => $this->order->get('field_shipping_vendor')->isEmpty() ?
-      NULL : $this->order->get('field_shipping_vendor')->value,
-    ];
-
-    $pane_form['field_shipping_account_number'] = [
-      '#type' => 'textfield',
-      '#title' => $this->order->get('field_shipping_account_number')->getFieldDefinition()->getLabel(),
-      '#default_value' => $this->order->get('field_shipping_account_number')->isEmpty() ?
-      NULL : $this->order->get('field_shipping_account_number')->value,
-    ];
-
+  public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
     $pane_form['setting'] = [
       '#type' => 'select',
       '#title' => $this->order->get('field_setting')->getFieldDefinition()->getLabel(),
@@ -58,8 +40,6 @@ class CustomerQuestions extends CheckoutPaneBase implements CheckoutPaneInterfac
    */
   public function submitPaneForm(array &$pane_form, FormStateInterface $form_state, array &$complete_form) {
     $value = $form_state->getValue($pane_form['#parents']);
-    $this->order->set('field_shipping_vendor', $value['field_shipping_vendor']);
-    $this->order->set('field_shipping_account_number', $value['field_shipping_account_number']);
     $this->order->set('field_setting', $value['setting']);
   }
 
@@ -73,24 +53,6 @@ class CustomerQuestions extends CheckoutPaneBase implements CheckoutPaneInterfac
         '#title' => $this->t('Additional Information'),
       ],
     ];
-
-    if (!$this->order->get('field_shipping_vendor')->isEmpty()) {
-
-      $build['summary_display']['field_shipping_vendor'] = [
-        '#type' => 'item',
-        '#title' => $this->order->get('field_shipping_vendor')->getFieldDefinition()->getLabel(),
-        '#markup' => '<p>' . $this->order->get('field_shipping_vendor')->value . '</p>',
-      ];
-    }
-
-    if (!$this->order->get('field_shipping_account_number')->isEmpty()) {
-
-      $build['summary_display']['field_shipping_account_number'] = [
-        '#type' => 'item',
-        '#title' => $this->order->get('field_shipping_account_number')->getFieldDefinition()->getLabel(),
-        '#markup' => '<p>' . $this->order->get('field_shipping_account_number')->value . '</p>',
-      ];
-    }
 
     if (!$this->order->get('field_setting')->isEmpty()) {
 
