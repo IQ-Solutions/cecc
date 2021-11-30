e3d3bc267cead01d29eb1a5ce7de0405c2ebeb2f
krice <krice@iqsolutions.com>
2021-11-30 15:28:13 -0500

    adds restocked module

---
+++ modules/cecc_restocked/src/Ajax/ConfirmRegisterCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\cecc_restocked\Ajax;
+
+use Drupal\Core\Ajax\CommandInterface;
+
+/**
+ * Ajax Command for Confirming if user wants to register.
+ */
+class ConfirmRegisterCommand implements CommandInterface {
+
+}
