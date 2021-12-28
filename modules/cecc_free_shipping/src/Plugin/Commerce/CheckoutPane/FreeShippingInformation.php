08d0eaebca9f587932306bc68bb324c3156dafa5
krice <krice@iqsolutions.com>
2021-12-28 11:11:08 -0500

    adds free shipping module

---
+++ modules/cecc_free_shipping/src/Plugin/Commerce/CheckoutPane/FreeShippingInformation.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\cecc_free_shipping\Plugin\Commerce\CheckoutPane;
+
+use Drupal\commerce\InlineFormManager;
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutFlow\CheckoutFlowInterface;
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneBase;
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides the billing information pane.
+ *
+ * @CommerceCheckoutPane(
+ *   id = "cecc_free_shipping_information",
+ *   label = @Translation("Shipping information"),
+ *   default_step = "shipping_information",
+ *   wrapper_element = "fieldset",
+ * )
+ */
+class FreeShippingInformation extends CheckoutPaneBase implements CheckoutPaneInterface {
+
+
+  /**
+   * The inline form manager.
+   *
+   * @var \Drupal\commerce\InlineFormManager
+   */
+  protected $inlineFormManager;
+
+  /**
+   * Constructs a new BillingInformation object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\commerce_checkout\Plugin\Commerce\CheckoutFlow\CheckoutFlowInterface $checkout_flow
+   *   The parent checkout flow.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\commerce\InlineFormManager $inline_form_manager
+   *   The inline form manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CheckoutFlowInterface $checkout_flow, EntityTypeManagerInterface $entity_type_manager, InlineFormManager $inline_form_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $checkout_flow, $entity_type_manager);
+
+    $this->inlineFormManager = $inline_form_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, CheckoutFlowInterface $checkout_flow = NULL) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $checkout_flow,
+      $container->get('entity_type.manager'),
+      $container->get('plugin.manager.commerce_inline_form')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPaneSummary() {
+    $summary = [];
+    if ($profile = $this->order->get('cecc_shipping_profile')->entity) {
+      $profile_view_builder = $this->entityTypeManager->getViewBuilder('profile');
+      $summary = $profile_view_builder->view($profile, 'default');
+    }
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $profile = $this->order->get('cecc_shipping_profile')->entity;
+    if (!$profile) {
+      $profile_storage = $this->entityTypeManager->getStorage('profile');
+      $profile = $profile_storage->create([
+        'type' => 'customer',
+        'uid' => 0,
+      ]);
+    }
+    $inline_form = $this->inlineFormManager->createInstance('customer_profile', [
+      'profile_scope' => 'cecc_shipping',
+      'available_countries' => $this->order->getStore()->getBillingCountries(),
+      'address_book_uid' => $this->order->getCustomerId(),
+      // Don't copy the profile to address book until the order is placed.
+      'copy_on_save' => FALSE,
+    ], $profile);
+
+    $pane_form['profile'] = [
+      '#parents' => array_merge($pane_form['#parents'], ['profile']),
+      '#inline_form' => $inline_form,
+    ];
+    $pane_form['profile'] = $inline_form->buildInlineForm($pane_form['profile'], $form_state);
+
+    return $pane_form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitPaneForm(array &$pane_form, FormStateInterface $form_state, array &$complete_form) {
+    /** @var \Drupal\commerce\Plugin\Commerce\InlineForm\EntityInlineFormInterface $inline_form */
+    $inline_form = $pane_form['profile']['#inline_form'];
+    /** @var \Drupal\profile\Entity\ProfileInterface $profile */
+    $profile = $inline_form->getEntity();
+    $this->order->set('cecc_shipping_profile', $profile);
+  }
+
+}
